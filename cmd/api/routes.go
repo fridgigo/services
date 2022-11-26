7bf64b02a4684b1a2825cfa314a012f090dd89ff
Jeyhun Rahimli <rahimlijeyhun77@gmail.com>
2022-11-26 14:32:19 +0100

    add: creating a basic user routes

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -15,6 +15,7 @@ func Routes() *gin.Engine {
 		v1.GET("api/ping", handlers.Ping)
 		v1.POST("/api/users/login", handlers.Login)
 		v1.POST("/api/users/register", handlers.Register)
+		v1.GET("/api/users/user-info", handlers.GetUser)
 	}
 
 	return r
--- handlers/user_handlers.go
+++ handlers/user_handlers.go
@@ -1,15 +1,31 @@
 package handlers
 
-import "github.com/gin-gonic/gin"
+import (
+	"net/http"
+
+	"github.com/fridgigo/sercies/models"
+	"github.com/gin-gonic/gin"
+)
 
 /*
 post method
 user sign in function
 */
 func Login(c *gin.Context) {
-	c.JSON(200, gin.H{
-		"message": "Login endpoint",
-	})
+	var user models.User
+
+	if err := c.ShouldBindJSON(&user); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	// TODO:
+	// connect to db and check for user exists
+	// if user exits and password is correct
+	// then generate a json web token (jwt)
+	// return it back as a response
+	// if user not exists in db or password is wrong
+	// then send back error message as a response
 }
 
 /*
@@ -17,7 +33,37 @@ post method
 user sign up function
 */
 func Register(c *gin.Context) {
-	c.JSON(200, gin.H{
-		"message": "Register endpoint",
-	})
+	var user models.User
+
+	if err := c.ShouldBindJSON(&user); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	// TODO:
+	// connect to db and check for user exists
+	// if user exits in db
+	// return the error message back as a response
+	// if user not exists in db
+	// then insert in into db
+}
+
+/*
+get method
+get user infos
+*/
+func GetUser(c *gin.Context) {
+	var user models.User
+
+	if err := c.ShouldBindJSON(&user); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	// TODO:
+	// connect to db and check for user exists
+	// if user exits
+	// then response the user datas
+	// if user not exist
+	// then return back the error message as a response
 }
